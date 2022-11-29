a0d9f439f7baee4448f5e666c8a590369e5a42e9
JYuan <13826506407@139.com>
2022-11-29 22:09:36 +0800

    保存数据

--- src/pages/Polyline/index.tsx
+++ src/pages/Polyline/index.tsx
@@ -1,5 +1,6 @@
 import { Cesium } from '@sl-theia/vis';
 import { useEffect, useRef, useState } from 'react';
+import { setFlyLine } from './setFlyLine'
 import styles from './index.less';
 
 export default function Map(props: any) {
@@ -26,6 +27,7 @@ export default function Map(props: any) {
         fullscreenButton: false,
       });
       viewerRef.current = viewer;
+      viewer.scene.postProcessStages.fxaa.enabled = true;
 
       // 取消双击事件
       viewer.cesiumWidget.screenSpaceEventHandler.removeInputAction(
@@ -45,56 +47,182 @@ export default function Map(props: any) {
         destination: Cesium.Cartesian3.fromDegrees(110.2, 34.55, 3000000),
       });
 
-      var controls = [];
+      let controls = [];
       controls.push(
         Cesium.Cartesian3.fromDegrees(120.11551237582385, 35.97934910503657),
       );
       controls.push(Cesium.Cartesian3.fromDegrees(122.1367529, 36.9629172));
       controls.push(Cesium.Cartesian3.fromDegrees(124.1367529, 35.9629172));
 
-      setHermiteSpline(viewer, controls);
+      drawLine1(viewer);
+      drawLine2(viewer);
+      drawLine3(viewer);
+      drawLine4(viewer);
 
       setIsLoadedViewer(true);
     }
   }, []);
 
-  const setHermiteSpline = (viewer: Cesium.Viewer, controls: any[]) => {
-    //创建多段线和每段颜色
-    const positions = [];
-    const colors = [];
-    for (let j = 0; j <= 50; j += 5) {
-      positions.push(
-        Cesium.Cartesian3.fromDegrees(-124.0 + j, 40, 50000.0 * (j % 10)),
-      );
+  const drawLine1 = (viewer: Cesium.Viewer) => {
+    let positions = [];
+    let colors = [];
+
+    for (let i = 0; i < 40; ++i) {
+      positions.push(Cesium.Cartesian3.fromDegrees(-100.0 + i, 6.0));
       colors.push(Cesium.Color.fromRandom({ alpha: 1.0 }));
     }
-    //设置线段的位置和颜色，一一对应，arcType为ArcType.NONE
-    const perSegmentPolyline = new Cesium.GeometryInstance({
-      geometry: new Cesium.SimplePolylineGeometry({
-        positions: positions,
-        colors: colors,
-        arcType: Cesium.ArcType.NONE,
-      }),
-    });
-    //使用逐顶点着色绘制多段线
-    //对于逐顶点着色，将colorsPerVertex选项设置为true，并为colors选项提供长度等于位置数的颜色数组
-    const perVertexPolyline = new Cesium.GeometryInstance({
-      geometry: new Cesium.PolylineGeometry({
-        positions: Cesium.Cartesian3.fromDegreesArray([-100, 40, -80, 30]),
-        colors: [Cesium.Color.RED, Cesium.Color.BLUE],
-        colorsPerVertex: true,
-      }),
-    });
-    //添加多段线instances到primitives
+
     viewer.scene.primitives.add(
       new Cesium.Primitive({
-        geometryInstances: [perSegmentPolyline, perVertexPolyline],
+        geometryInstances: new Cesium.GeometryInstance({
+          geometry: new Cesium.SimplePolylineGeometry({
+            positions: positions,
+            colors: colors,
+          }),
+        }),
         appearance: new Cesium.PerInstanceColorAppearance({
           flat: true,
           renderState: {
-            lineWidth: Math.min(2.0, viewer.scene.maximumAliasedLineWidth),
+            lineWidth: Math.min(4.0, viewer.scene.maximumAliasedLineWidth),
           },
         }),
+      }),
+    );
+  };
+
+  const drawLine2 = (viewer: Cesium.Viewer) => {
+    let positions = [];
+    let colors = [];
+    const number1 = 60;
+    const number2 = 30;
+    const number3 = 60;
+    const number4 = 30;
+    const allNumber = 60 + 30 + 60 + 30;
+    for (let i = 0; i < number1; ++i) {
+      positions.push(Cesium.Cartesian3.fromDegrees(-110.0 + i, -15.0));
+    }
+
+    for (let i = 0; i < number2; ++i) {
+      positions.push(Cesium.Cartesian3.fromDegrees(-110.0 + 60, -15.0 + i));
+    }
+
+    for (let i = 0; i < number3; ++i) {
+      positions.push(
+        Cesium.Cartesian3.fromDegrees(-110.0 + 60 - i, -15.0 + 30),
+      );
+    }
+
+    for (let i = 0; i < number4; ++i) {
+      positions.push(
+        Cesium.Cartesian3.fromDegrees(-110.0 + 60 - 60, -15.0 + 30 - i),
+      );
+    }
+
+    console.warn('positions.length', positions.length);
+    console.warn('allNumber', allNumber);
+    for (let i = 0; i < allNumber; ++i) {
+      let alpha = 0;
+      if (i < allNumber / 2) {
+        alpha = (i / allNumber) * 2 * 0.6 + 0.2;
+      } else {
+        alpha = ((allNumber - i) / allNumber) * 2 * 0.6 + 0.2;
+      }
+      console.warn('alpha', alpha);
+      colors.push(
+        Cesium.Color.fromRandom({
+          red: 0.0,
+          green: 1.0,
+          blue: 0.0,
+          alpha: alpha,
+        }),
+      );
+    }
+
+    viewer.scene.primitives.add(
+      new Cesium.Primitive({
+        geometryInstances: new Cesium.GeometryInstance({
+          geometry: new Cesium.PolylineGeometry({
+            positions: positions,
+            width: 8.0,
+            vertexFormat: Cesium.PolylineColorAppearance.VERTEX_FORMAT,
+            colors: colors,
+            colorsPerVertex: true,
+          }),
+        }),
+        appearance: new Cesium.PolylineColorAppearance(),
+      }),
+    );
+
+    setFlyLine(viewer, positions)
+  };
+
+  const drawLine3 = (viewer: Cesium.Viewer) => {
+    let positions = [];
+    for (let i = 0; i < 40; ++i) {
+      positions.push(Cesium.Cartesian3.fromDegrees(-100.0 + i, 10.0));
+    }
+
+    let image = require('./colors.png');
+    let material = new Cesium.Material({
+      fabric: {
+        type: 'EmissionMap',
+        uniforms: {
+          image: image,
+          repeat: {
+            x: 1,
+            y: 1,
+          },
+        },
+        components: {
+          diffuse: 'texture2D(image, materialInput.st).rgb',
+          alpha: 'texture2D(image, materialInput.st).a',
+        },
+      },
+      translucent: function () {
+        return true;
+      },
+    });
+
+    viewer.scene.primitives.add(
+      new Cesium.Primitive({
+        geometryInstances: new Cesium.GeometryInstance({
+          geometry: new Cesium.PolylineGeometry({
+            positions: positions,
+            width: 8.0,
+            vertexFormat: Cesium.PolylineMaterialAppearance.VERTEX_FORMAT,
+          }),
+        }),
+        appearance: new Cesium.PolylineMaterialAppearance({
+          material: material,
+        }),
+      }),
+    );
+  };
+
+  const drawLine4 = (viewer: Cesium.Viewer) => {
+    let positions = [];
+    let colors = [];
+    for (let i = 0; i < 40; ++i) {
+      positions.push(Cesium.Cartesian3.fromDegrees(-100.0 + i, 12.0));
+      colors.push(Cesium.Color.fromRandom({ alpha: 1.0 }));
+    }
+
+    for (let i = 0; i < 40; ++i) {
+      positions.push(Cesium.Cartesian3.fromDegrees(-100 + 39, 12.0 + i));
+      colors.push(Cesium.Color.fromRandom({ alpha: 1.0 }));
+    }
+
+    viewer.scene.primitives.add(
+      new Cesium.Primitive({
+        geometryInstances: new Cesium.GeometryInstance({
+          geometry: new Cesium.PolylineGeometry({
+            positions: positions,
+            width: 4.0,
+            vertexFormat: Cesium.PolylineColorAppearance.VERTEX_FORMAT,
+            colors: colors,
+          }),
+        }),
+        appearance: new Cesium.PolylineColorAppearance(),
       }),
     );
   };
